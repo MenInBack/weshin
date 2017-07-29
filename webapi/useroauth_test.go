58b6eb3c97d0715a1a6cbbe4d49989806146c91c
houz <jijiaoqi@gmail.com>
2017-07-29 12:23:42 +0800

    test update

--- webapi/useroauth_test.go
+++ webapi/useroauth_test.go
@@ -48,14 +48,14 @@ func (s *sampleStorage) GetAccessToken() string {
 	return s.token
 }
 
-func (s *sampleStorage) SetAPITicket(ticket *wx.APITicket) {
+func (s *sampleStorage) SetJSTicket(ticket *wx.APITicket) {
 	if ticket.Typ == wx.TicketTypeJSAPI {
 		log.Println("jsapi ticket setted: ", ticket)
 		s.jsAPITicket = ticket.Ticket
 	}
 }
 
-func (s *sampleStorage) GetJSAPITicket() *wx.APITicket {
+func (s *sampleStorage) GetJSTicket(string) *wx.APITicket {
 	return &wx.APITicket{
 		Ticket: s.jsAPITicket,
 	}
